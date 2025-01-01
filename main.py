6de2ab5c6545b110bc501420b2eb83ab288f1378
Philip William Ventura <philipwilliamventura@gmail.com>
2024-12-31 19:39:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import speech_recognition as sr
+import os
+import time
+import playsound
+from gtts import gTTS
+
+def interviewer(text):
+    tts = gTTS(text=text)
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
 
 def get_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
-        said = ""
+        answer = ""
         try:
-            said = r.recognize_google(audio)
-            print(said)
+            answer = r.recognize_google(audio)
+            print(answer)
         except Exception as e:
             print("Exception: " + str(e))
-
-    return said
-
-get_audio()
+    return answer
